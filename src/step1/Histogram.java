f93d585a1a673dba78261ad15440a90468a8772c
jamminsoccerguy <msplucker@gmail.com>
2016-02-17 13:46:42 -0600

    Updated/fixed step1 and step2

--- src/step1/Histogram.java
+++ src/step1/Histogram.java
@@ -21,8 +21,8 @@ public class Histogram {
 
         //validates that n is in the acceptable range
         if(n < 0 || n > 20){
-            //System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and 20");
-            throw new IllegalArgumentException("Error: Integer is not in the Acceptable Range");
+            System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and 20");
+            //throw new IllegalArgumentException("Error: Integer is not in the Acceptable Range");
         }
         else{
             histogramLength = n;
@@ -36,8 +36,8 @@ public class Histogram {
 
         //validates that i is in the acceptable range
         if(i < 0 || i >= histogramLength){
-            //System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and " + (histogramLength - 1));
-            throw new IllegalArgumentException("Error: Integer is not in the Acceptable Range");
+            System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and " + (histogramLength - 1));
+            //throw new IllegalArgumentException("Error: Integer is not in the Acceptable Range");
         }
         else{
             array[i]++; //adds the value to the histogram
@@ -46,34 +46,20 @@ public class Histogram {
 
 
     //print out the contents of the histogram using "*" for each occurrence of a value
-    public void displayHistogram(String option){
+    public void displayHistogram(){
 
         //local variable
         String output = "Element\t #\tHistogram";
 
-        //long option prints all histogram values (0...n)
-        if(option.equalsIgnoreCase("l")){
-            for(int i = 0; i < array.length; i++){ //will print out a row for each element in the array
-                output += "\n" + i + "\t " + array[i] + "\t"; //adds the element and occurrence of each value to the output
+        //prints all histogram values (0...n)
+        for(int i = 0; i < array.length; i++){ //will print out a row for each element in the array
+            output += "\n   " + i + "\t " + array[i] + "\t"; //adds the element and occurrence of each value to the output
 
-                for(int j = 0; j < array[i]; j++){ //will print an asterisk for each occurrence of a value
-                    output += "*"; //adds bar of asterisks to output
-                }
+            for(int j = 0; j < array[i]; j++){ //will print an asterisk for each occurrence of a value
+                output += "*"; //adds bar of asterisks to output
             }
-            System.out.println(output); //print out histogram
         }
-
-        //short option will print all histogram values, but will skip any 0 values
-        else if(option.equalsIgnoreCase("s")){
-            for(int i = 1; i < array.length; i++){ //will print out a row for each element in the array (skips "0" values)
-                output += "\n" + i + "\t " + array[i] + "\t"; //adds the element and occurrence of each value to the output
-
-                for(int j = 0; j < array[i]; j++){ //will print an asterisk for each occurrence of a value
-                    output += "*"; //adds bar of asterisks to output
-                }
-            }
-            System.out.println(output); //print out histogram
-        }
+        System.out.println(output); //print out histogram
     }
 
 
@@ -109,12 +95,7 @@ public class Histogram {
         histo.add(9);
 
 
-        //prints out long display of the histogram
-        histo.displayHistogram("L");
-
-        System.out.println();
-
-        //prints out short display of the histogram
-        histo.displayHistogram("S");
+        //prints out display of the histogram
+        histo.displayHistogram();
     }
 }
--- src/step2/DoubleIt.java
+++ src/step2/DoubleIt.java
@@ -13,7 +13,6 @@ public class DoubleIt {
 
     //instance variables
     private int [] array;
-    private int [] newArray;
     private int histogramLength;
 
 
@@ -21,9 +20,9 @@ public class DoubleIt {
     public DoubleIt(int n){
 
         //validates that n is in the acceptable range
-        if(n < 0 || n > 100){
-            System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and 100");
-            //throw new Exception("Integer is not in the Acceptable Range");
+        if(n < 0 || n > 20){
+            System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and 20");
+            //throw new IllegalArgumentException("Error: Integer is not in the Acceptable Range");
         }
         else{
             histogramLength = n;
@@ -38,6 +37,7 @@ public class DoubleIt {
         //validates that i is in the acceptable range
         if(i < 0 || i >= histogramLength){
             System.out.println("Error: Integer is not in the acceptable range. Must be between 0 and " + (histogramLength - 1));
+            //throw new IllegalArgumentException("Error: Integer is not in the Acceptable Range");
         }
         else{
             array[i]++; //adds the value to the histogram
@@ -48,9 +48,13 @@ public class DoubleIt {
     //automatically extends the size of the histogram
     public void addAuto(int i){
 
+        //local variable
+        int [] newArray;
+
         //validates that i is a positive number
         if(i < 0){
-            System.out.println("Error: Integer must be a postive number.");
+            System.out.println("Error: Integer must be a positive number.");
+            //throw new IllegalArgumentException("Error: Integer must be a positive number.");
         }
         else{
             newArray = new int[(array.length * 2)]; //creates the newArray, but doubles the space of the old array
@@ -68,34 +72,20 @@ public class DoubleIt {
 
 
     //print out the contents of the histogram using "*" for each occurrence of a value
-    public void display(String option){
+    public void displayHistogram(){
 
         //local variable
         String output = "Element\t #\tHistogram";
 
-        //long option prints all histogram values (0...n)
-        if(option.equalsIgnoreCase("l")){
-            for(int i = 0; i < array.length; i++){ //will print out a row for each element in the array
-                output += "\n" + i + "\t " + array[i] + "\t"; //adds the element and occurrence of each value to the output
+        //prints all histogram values (0...n)
+        for(int i = 0; i < array.length; i++){ //will print out a row for each element in the array
+            output += "\n   " + i + "\t " + array[i] + "\t"; //adds the element and occurrence of each value to the output
 
-                for(int j = 0; j < array[i]; j++){ //will print an asterisk for each occurrence of a value
-                    output += "*"; //adds bar of asterisks to output
-                }
+            for(int j = 0; j < array[i]; j++){ //will print an asterisk for each occurrence of a value
+                output += "*"; //adds bar of asterisks to output
             }
-            System.out.println(output); //print out histogram
         }
-
-        //short option will print all histogram values, but will skip any 0 values
-        else if(option.equalsIgnoreCase("s")){
-            for(int i = 1; i < array.length; i++){ //will print out a row for each element in the array (skips "0" values)
-                output += "\n" + i + "\t " + array[i] + "\t"; //adds the element and occurrence of each value to the output
-
-                for(int j = 0; j < array[i]; j++){ //will print an asterisk for each occurrence of a value
-                    output += "*"; //adds bar of asterisks to output
-                }
-            }
-            System.out.println(output); //print out histogram
-        }
+        System.out.println(output); //print out histogram
     }
 
 
@@ -132,13 +122,8 @@ public class DoubleIt {
         histo.add(9);
 
 
-        //prints out long display of the histogram
-        histo.display("L");
-
-        System.out.println();
-
-        //prints out short display of the histogram
-        histo.display("S");
+        //prints out display of the histogram
+        histo.displayHistogram();
 
 
         //test new addAuto method
@@ -147,6 +132,6 @@ public class DoubleIt {
 
         System.out.println();
 
-        histo.display("l"); //prints out long display of the histogram
+        histo.displayHistogram(); //prints out display of the histogram
     }
 }
